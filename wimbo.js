19718671b846a5200f84dd0a0f2c4c3f51555daf
korasin <mchemutai232@gmail.com>
2022-05-24 22:00:42 +0300

    add deletion

--- wimbo.js
+++ wimbo.js
@@ -1,43 +1,61 @@
 // console.log('loaded')
 
-let myList = ['Ann','Lilian','Joy','Nzau'];
+//create a array of names
+let namesOfRegisteredPersons = ['Ann','Lilian','Joy','Nzau'];
 // console.log(myList);
 
-let nameKnown = [];
+//create a new empty array to hold the names to be entered by user
+let personsWhoHaveEaten = [];
 // console.log(nameKnown);
 
+//define submit function
 function submit(){
     // console.log(submit);
 
-    let newList = document.getElementById('result').value;
+    //getting the name entered by user in html
+    let nameEnteredByUser = document.getElementById('result').value;
     // console.log(newList);
 
-    let nameList = myList.includes(newList);
+    //checking if name given by user is included in the array myListi
+    let ifNameIsIncluded = namesOfRegisteredPersons.includes(nameEnteredByUser);
     // console.log(nameList);
      
-    if (nameList){
+    //if name is included in myList array
+    if (ifNameIsIncluded){
         // console.log('You are registered');
-        $('#kira').html("You are registered")
+        
+        //getting name  using id regist to give output
+        $('#regist').html("You are registered")
          
-
-        let personEaten = nameKnown.includes(newList)
+        //checking if name entered by user is included in the array of people who have eaten
+        let personEaten = personsWhoHaveEaten.includes(nameEnteredByUser)
         // console.log(personEaten);
 
-     nameKnown.push(newList)
+    //  nameKnown.push(nameEnteredByUser)
         // console.log(nameKnown);
 
+        //checking if the person has eaten
         if (personEaten){
             // console.log("You have eaten");
-            $("#kara").html("You have eaten")
+
+            //getting name using id ifeaten to give output
+            $("#ifeaten").html("You have eaten")
         }else{
             // console.log("You haven't eaten");
-            $("#kara").html("You haven't eaten")
+
+            //getting name using id ifeaten to give output
+            $("#ifeaten").html("You haven't eaten")
+
+            //pushing name to persons who have eaten if person is taking meal for the first time
+            personsWhoHaveEaten.push(nameEnteredByUser)
         }
      
 
     }else{
         // console.log('you are not registered');
-        $("#kira").html("You are not registered")
+
+        //getting name using id regist to give output
+        $("#regist").html("You are not registered")
     }
 }
 
@@ -49,8 +67,12 @@ function submit(){
 // document.getElementById("reset").reset();
 // document.forms[0].reset();
 
-function kiki(){
-    // console.log(kiki)
+
+//define reset function
+function reset(){
+    // console.log(reset)
+
+    //getting value from html by id result and reset the string to empty
     document.getElementById('result').value = '';
 
 //     location.reload();
